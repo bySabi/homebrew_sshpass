14179d0aa6de701e31411ea9f2e5fa55b2a6b04d
bySabi Files <flxinformatico@gmail.com>
2015-01-26 19:13:38 +0100

    update URL

--- sshpass.rb
+++ sshpass.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Sshpass < Formula
-  url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.05/sshpass-1.05.tar.gz'
+  url 'http://www.mirrorservice.org/sites/downloads.sourceforge.net/s/ss/sshpass/sshpass/1.05/sshpass-1.05.tar.gz'
   homepage 'http://sourceforge.net/projects/sshpass'
-  md5 'c2e14084669be147fbd6e3532949dbfe'
+  sha1 'c2e14084669be147fbd6e3532949dbfe'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
